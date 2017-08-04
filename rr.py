60b87a6d88e021e265209eb0f02622f92e86f0e7
Prabhat Kumar Jha <prabhuyesjha@gmail.com>
2017-08-04 16:39:55 -0500

    Add files via upload

---
+++ rr.py
@@ -0,0 +1,10 @@
+import datetime
+
+x=  datetime.date.today() - datetime.datetime.strptime('2014-12-31', "%Y-%m-%d").date()
+print x.days
+y=134
+y=y.__str__()
+print type(y)vi
+print datetime.date(2016, 7, 1)
+
+
